4ec4367514d57a1cae86d7af5877726d225fb739
arafat717 <arafatjibon33@gmail.com>
2025-02-20 23:21:30 +0600

    added interceptor for logout unauthorized and forbiden access

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,35 @@
 import axios from "axios";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { useNavigate } from "react-router-dom";
 const AxiosSecureInstance = axios.create({
   baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 const useAxiosSecure = () => {
+  const { handleLogOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    AxiosSecureInstance.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      function (error) {
+        // console.log("error from interceptor:", error);
+        if (error.status === 401 || error.status === 403) {
+          handleLogOut()
+            .then(() => {
+              console.log("loggedOut");
+              navigate("/login");
+            })
+            .catch((error) => console.log(error));
+        }
+
+        return Promise.reject(error);
+      }
+    );
+  }, []);
+
   return AxiosSecureInstance;
 };
 
